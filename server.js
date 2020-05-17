a3e780941103d498bbd066b29c06aa645ba0a62f
Bounty556 <jacobmayday@gmail.com>
2020-05-16 23:22:47 -0600

    reset file structure

--- server.js
+++ server.js
@@ -58,12 +58,12 @@ app.delete('/api/notes/:id', (req, res) => {
 
 // Send notes.html for /notes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 // Send index.html for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, function() {
